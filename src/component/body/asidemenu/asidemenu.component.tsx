dd4063471860d0947b5f36a5bd36eaf9452f5afd
Kai Wang <kaiuchihaa@gmail.com>
2023-05-11 22:55:27 -0300

    adicionado links para contatos

--- src/component/body/asidemenu/asidemenu.component.tsx
+++ src/component/body/asidemenu/asidemenu.component.tsx
@@ -11,19 +11,19 @@ const AsideMenu = () => {
             <ul className="aside-nav-menu">
 
                 <li className="aside-nav-menu-item">
-                    <a href="#linkdin" className="aside-menu-item--link" target={"blank"}>
+                    <a href="https://www.linkedin.com/in/deusnir-portela-633b90181/" className="aside-menu-item--link" target={"blank"}>
                         <AiFillLinkedin/>
                     </a>
                 </li>
 
                 <li className="aside-nav-menu-item">
-                    <a href="#github" className="aside-menu-item--link" target={"blank"}>
+                    <a href="https://github.com/xing-wang-kai" className="aside-menu-item--link" target={"blank"}>
                         <AiFillGithub />
                     </a>
                 </li>
 
                 <li className="aside-nav-menu-item">
-                    <a href="#api-whatsapp" className="aside-menu-item--link" target={"blank"}>
+                    <a href="https://wa.me/5511990150864?text=%3Ckoder%3E+Logo+vou+te+atender+%5E%5E%2F" className="aside-menu-item--link" target={"blank"}>
                      <FaWhatsappSquare />
                     </a>
                 </li>
